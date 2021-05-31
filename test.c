c6bec1ac19d7be00b0dc2091ae0dbc5d1a507112
Shota Suginouchi <>
2021-05-31 07:41:54 +0000

    課題1ができたのでコミット

--- test.c
+++ test.c
@@ -1,5 +1,5 @@
-#include <cstdio>
-int main()
+#include <stdio.h>
+int main(void)
 {
     int a,b;
     int c;
@@ -7,4 +7,9 @@ int main()
     b=2;
     c=a+b;
     printf("%d\n", c);
+
+    return 0;
+
+    // たとえばここにコードを追加する
+    //　課題1hogehoge
 }
